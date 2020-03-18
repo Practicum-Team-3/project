67d890a52e35dea9cd208ef13f41c82fab41729f
Danner Pacheco <djpachecoontiveros@miners.utep.edu>
2020-03-18 15:46:34 -0600

    Working Celery Test, DO NOT MERGE

--- src/server.py
+++ src/server.py
@@ -1,10 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, url_for
 from model.ScenarioManager import ScenarioManager
 from model.VagrantManager import VagrantManager
+
+from celery import Celery
 
 app = Flask(__name__)
 scenario_manager = ScenarioManager()
 vagrant_manager = VagrantManager()
+
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 @app.route('/scenarios/all')
 def getScenarios():
@@ -67,6 +75,12 @@ def runVagrantUp(scenario_name):
   """
   return jsonify(vagrant_manager.runVagrantUp(scenario_name))
 
+@celery.task(bind=True)
+def testCelery(self,scenario_name, source, destination):
+  return vagrant_manager.testNetworkPing(scenario_name, source, destination)
+  
+
+
 @app.route('/scenarios/<scenario_name>/ping/<source>/<destination>')
 def testPing(scenario_name, source, destination):
   """
@@ -76,7 +90,41 @@ def testPing(scenario_name, source, destination):
   :param destination: Destination virtual machine
   :return:
   """
-  return jsonify(vagrant_manager.testNetworkPing(scenario_name, source, destination))
+  task = testCelery.apply_async(scenario_name, source, destination)
+  return jsonify({}), 202, {'Location': url_for('taskstatus',
+                                                  task_id=task.id)}
+  
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
+
+
+
 
 if __name__=="__main__":
   app.run()
