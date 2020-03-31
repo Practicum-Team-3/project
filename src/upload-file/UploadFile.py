db10868c41bca40efd9c69724235bca5c101c947
revil0mg <oliver_mtz_g@yahoo.com>
2020-03-31 16:23:54 -0600

    Update UploadFile.py

--- src/upload-file/UploadFile.py
+++ src/upload-file/UploadFile.py
@@ -5,16 +5,23 @@ import os
 """ Modified from https://github.com/twtrubiks/flask-dropzone-wavesurfer"""
 
 uploadfiles = Flask(__name__)
-UPLOAD_PATH = 'static/uploads'
+UPLOAD_PATH = 'Files/'
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_PATH)
+#UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_PATH)
 UPLOAD_F = os.path.abspath(os.path.join(APP_ROOT, os.pardir))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_PATH)
 
 #TODO: Handle VagrantFiles, VMs, exploits (zip, 7z, etc.) or just everything?
 #TODO: get existing files??
 #TODO: Set appropiate paths to files. 
 #TODO: See what the scope of what I need to do is.
+#TODO: Vulnerable programs
 
+#TODO: POST parameter for the type of file 
+#TODO: Return file list in 
+#TODO: MAke a different folder to  add the stuff
+
+#TODO: DELETRION OF FILES
 @uploadfiles.route('/')
 def index():
     all_VMs = []
@@ -28,37 +35,37 @@ def index():
         ## Exploits
         if (isExploitFormat(filename)):
             all_exploits.append(filename)
-        ## VagrantFiles
-        if (isVagrantFileFormat(filename)):
-            all_vagrantfiles.append(filename)
             
-        if (filename.find('.png') > -1):
-            all_image_files.append(filename)
+
             
     return render_template('index.html', **locals());
 
 
 def isVMFormat(link):
     if (link.find('.ova') > -1):
-        return True;
-    return False;
+        return 'Virtual Machines';
+    return;
 
 def isExploitFormat(link):
     if (link.find('.txt') > -1 or link.find('.go') > -1 or link.find('.py') > -1):
-        return True;
-    return False;
+        return 'Exploits';
+    return;
+    
+def isVulnerabilityFormat(link):
+    if (link.find('.zip') > -1 or link.find('.tar.gz') > -1 or link.find('.rar') > -1 or link.find('.7z') > -1):
+        return 'Vulnerable Software';
+    return;
 
-#TODO: This method
-def isVagrantFileFormat(link):
-    if (link.find('.png') > -1):
-        return True;
-    return False;
+def filePath(link):
+    return UPLOAD_FOLDER + isVMFormat(link) + isExploitFormat(link) + isVulnerabilityFormat(link);
 
 @uploadfiles.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         file = request.files['file']
         upload_path = '{}/{}'.format(UPLOAD_FOLDER, secure_filename(file.filename))
+        print(UPLOAD_F)
+        print()
         print(UPLOAD_FOLDER)
         file.save(upload_path)
         return 'ok'
